bb0f73846fcfe01d32e0cd0da7a4ccfc533e1f1a
Vinh Bach Sy Duc <vinhbach@Vinhs-MacBook-Pro-2.local>
2018-03-27 21:41:28 +0800

    Update publish pacts script

---
+++ bin/publishPacts.js
@@ -0,0 +1,9 @@
+import pact from '@pact-foundation/pact-node'
+import path from 'path'
+
+let opts = {
+    consumerVersion: process.env.npm_package_version,
+    pactBroker: process.env.PACT_BROKER_URL,
+    pactFilesOrDirs: [path.resolve(process.cwd(), 'pacts')]
+}
+pact.publishPacts(opts).then(() => { done() })
